e2577698875882e19cd3d4564f5bec2e3514964c
Dave Hudson <dave@applification.net>
2022-01-31 13:06:32 +0000

    Fixing some rollup complaints

--- apps/blog/rollup.config.js
+++ apps/blog/rollup.config.js
@@ -1,14 +1,16 @@
 import commonJs from "@rollup/plugin-commonjs";
 import jsonPlugin from "@rollup/plugin-json";
-import resolve from "@rollup/plugin-node-resolve";
+import resolve, { nodeResolve } from "@rollup/plugin-node-resolve";
 
 export default {
   input: "api/api.js",
   output: {
+    exports: "default",
     file: "api/index.js",
     format: "cjs"
   },
   plugins: [
+    nodeResolve({ preferBuiltins: true}),
     commonJs(),
     resolve({
       moduleDirectories: ["node_modules"]
