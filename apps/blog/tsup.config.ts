865a08a755adb777cd42033a799d4bf4a1edfbe7
Dave Hudson <dave@applification.net>
2022-02-01 22:54:30 +0000

    change build order

--- apps/blog/tsup.config.ts
+++ apps/blog/tsup.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["api/index.ts"],
   outDir: "./api",
-  noExternal: ["../../node_modules/@remix-run/vercel"],
+  noExternal: ["@remix-run/vercel"],
 });
