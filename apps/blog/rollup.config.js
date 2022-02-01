e5773d8d1c01b8b153cf8aa92c0cc1eea366433f
Dave Hudson <dave@applification.net>
2022-02-01 16:15:15 +0000

    Different rollup

--- apps/blog/rollup.config.js
+++ apps/blog/rollup.config.js
@@ -1,20 +1,32 @@
-import commonJs from "@rollup/plugin-commonjs";
-import jsonPlugin from "@rollup/plugin-json";
 import resolve, { nodeResolve } from "@rollup/plugin-node-resolve";
+
+// export default {
+//   input: "api/api.js",
+//   output: {
+//     exports: "default",
+//     file: "api/index.js",
+//     format: "cjs"
+//   },
+//   plugins: [
+//     nodeResolve({ preferBuiltins: true}),
+//     commonJs(),
+//     resolve({
+//       moduleDirectories: ["node_modules"]
+//     }),
+//     jsonPlugin()
+//   ]
+// };
 
 export default {
   input: "api/api.js",
   output: {
-    exports: "default",
     file: "api/index.js",
-    format: "cjs"
+    format: 'esm'
   },
-  plugins: [
+    plugins: [
     nodeResolve({ preferBuiltins: true}),
-    commonJs(),
     resolve({
       moduleDirectories: ["node_modules"]
     }),
-    jsonPlugin()
   ]
 };
