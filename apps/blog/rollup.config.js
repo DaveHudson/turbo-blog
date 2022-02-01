66d5f57cd49db5d9816c7e8054b06629affd59da
Dave Hudson <dave@applification.net>
2022-02-01 22:17:33 +0000

    config change

--- apps/blog/rollup.config.js
+++ apps/blog/rollup.config.js
@@ -1,10 +1,17 @@
 import {nodeResolve} from "@rollup/plugin-node-resolve";
+import path from "path";
 
 export default {
   input: 'api/api.js',
   output: {
     file: 'api/index.js',
-    format: 'cjs'
   },
-  plugins: [nodeResolve({moduleDirectories: ['node_modules']})],
+  plugins: [
+    nodeResolve({
+      preferBuiltins: true,
+      rootDir: path.join(process.cwd(), '../..'),
+      moduleDirectories: ['node_modules'],
+      exportConditions: ['node']
+    })
+  ],
 };
